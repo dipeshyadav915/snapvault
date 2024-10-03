66e8b6aee4f32b5554bfdad47f08ed1bf5f20ab1
dipeshyadav915 <dipeshy499@gmail.com>
2024-10-03 15:04:47 +0530

    add password

--- db.php
+++ db.php
@@ -1,9 +1,10 @@
 <?php
 $host = "localhost";
 $username = "root";
-$password = "";
+$password = "1234";
 $database = "snapvault";
 $conn =  mysqli_connect($host, $username, $password, $database);
 if (!$conn) {
     die("Failed to connect to MySQL: " . mysqli_connect_error());
-} session_start();
+}
+session_start();
